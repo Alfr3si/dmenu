515a1ce39ffe946aa6953a600cfec4513bd9950b
Alfr3si <luisalfredolopez510@gmail.com>
2025-02-15 12:11:17 -0600

    chambio

--- config.h
+++ config.h
@@ -8,8 +8,7 @@ static int min_width = 400;                    /* minimum width when centered */
 static const float menu_height_ratio = 2.0f;  /* This is the ratio used in the original calculation */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-/*	"monospace:style=Bold:size=13"*/
-	  "monospace:style=Bold:size=14"
+	"JetBrainsMono Nerd Font:size=15"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
